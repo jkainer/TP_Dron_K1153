1cc0acb5aa67941b79a99591223b6861d47814a1
jkainer <jkainer@frba.utn.edu.ar>
2026-09-20 04:05:04 -0300

    Implementación del menú de cargado de ataque.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stdio.h>
+#define CURSOR "-> " // Cursor indicativo de interfaz. Puramente estético.
+#define RUTA_MAX 100 // Tamaño máximo en caracteres posible para ruta de un archivo.
 using std::cin, std::cout, std::endl;
 
 // PascalCase para estructuras.
@@ -28,9 +31,34 @@ struct Orden {
 	int siguiente_y 		= 0;
 };
 
+// TODO: Cargar y validar registros del archivo.
+void cargarAtaque() {
+	FILE *archivo_ataque;
+	char direccion_archivo[RUTA_MAX];
+
+	cout << "Por favor, indique ruta del archivo de ataque a cargar." << endl;
+	while (true) {
+		cout << CURSOR;
+		// Se utiliza getline en vez de cin para poder incluir espacios
+		// en la ruta del archivo. Se usan 2 para evitar un error de leading whitespace.
+		cin.getline(direccion_archivo, RUTA_MAX);
+		cin.getline(direccion_archivo, RUTA_MAX);
+		//cin >> direccion_archivo;
+
+		if (archivo_ataque = fopen(direccion_archivo, "r+b")) break;
+
+		cout << "La dirección del archivo ingresada es inválida o el archivo no existe. ";
+		cout << "Por favor, vuelva a intentarlo o ingrese una ruta válida." << endl;
+	};
+
+	cout << "Archivo válido." << endl;
+	fclose(archivo_ataque);
+	return;
+}
+
 
 void bucleMenu() {
-	unsigned int eleccion = 0;
+	int eleccion = 0;
 
 	do {
 		// Con esta comparación se evita reimprimir el menú si
@@ -38,18 +66,19 @@ void bucleMenu() {
 		// acaba de salir de una opción real luego de utilizarla.
 		if (eleccion < 7) {
 			cout << "Menú de opciones de control de memoria del dron." << endl;
+			cout << endl;
 			cout << "Por favor, selecciones una de las siguientes opciones: (ej: 1, 2, 3)" << endl;
-			cout << "	[1]. Cargar archivo de ataque a memoria." << endl;
-			cout << "	[2]. Mostrar archivo cargado." << endl;
-			cout << "	[3]. Crear un archivo de ataque nuevo." << endl;
-			cout << "	[4]. Corregir un registro del archivo." << endl;
-			cout << "	[5]. Corregir un registro en memoria." << endl;
-			cout << "	[6]. Guardar en memoria un archivo nuevo." << endl;
-			cout << "	[7]. Visualizar un archivo de ataque en HTML." << endl;
-			cout << "	[0]. Terminar este programa." << endl;
+			cout << "  [1]. Cargar archivo de ataque a memoria." << endl;
+			cout << "  [2]. Mostrar archivo cargado." << endl;
+			cout << "  [3]. Crear un archivo de ataque nuevo." << endl;
+			cout << "  [4]. Corregir un registro del archivo." << endl;
+			cout << "  [5]. Corregir un registro en memoria." << endl;
+			cout << "  [6]. Guardar en memoria un archivo nuevo." << endl;
+			cout << "  [7]. Visualizar un archivo de ataque en HTML." << endl;
+			cout << "  [0]. Terminar este programa." << endl;
 		}
 
-		cout << "-> ";
+		cout << CURSOR;
 		cin >> eleccion;
 		switch (eleccion) {
 			default:
@@ -59,7 +88,7 @@ void bucleMenu() {
 					cout << "Terminando ejecución." << endl;
 					break;
 				case 1:
-					cout << "Opción 1." << endl;
+					cargarAtaque();
 					break;
 				case 2:
 					cout << "Opción 2." << endl;
